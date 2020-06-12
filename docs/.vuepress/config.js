d6d7f9995f36f246a199d0fbd283bbcebc13b734
qaz963li <romens@romens-ckg.local>
2020-06-12 17:55:40 +0800

    PWA兼容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,21 @@ module.exports = {
     bath:"/",
     title:"blog for L",
     description:"在前端的路上疯狂踩坑",
+    head: [
+        ['link', { rel: 'icon', href: `/favicon.ico` }],
+        //增加manifest.json
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ],
+    plugins:[
+        '@vuepress/pwa',
+        {
+            serviceWorker: true,
+            updatePopup: true
+        }
+    ],
     themeConfig:{
         nav:[
             { text: "主页", link: "/" },
